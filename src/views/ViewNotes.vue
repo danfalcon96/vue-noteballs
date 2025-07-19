a047c40e95a7d30880b73630ceaa750b164ae127
Dan Falcon <dan_1196@yahoo.com>
2025-07-19 16:52:52 +0800

    feat: add pages for view of notes and stats

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="notes">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ViewStats.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="stats">
+    <h1>Stats</h1>
+  </div>
+</template>
+
+<style scoped></style>
