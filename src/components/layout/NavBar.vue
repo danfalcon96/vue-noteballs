2cc0b3091b0d1077cd3acf9a10a390a181d6d006
Dan Falcon <dan_1196@yahoo.com>
2025-07-19 18:25:00 +0800

    feat: create menu for mobile view

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,13 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const showMobileNav = ref(false)
+</script>
 
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
     <div class="container is-max-desktop">
       <div class="navbar-brand">
         <div class="navbar-item is-size-4 is-family-monospace">Noteballs</div>
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-expanded="false"
+          aria-label="menu"
+          data-target="noteballs-main-menu"
+          role="button"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <div id="noteballs-main-menu" class="navbar-menu">
+      <div id="noteballs-main-menu" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
         <div class="navbar-end">
           <RouterLink to="/" class="navbar-item" active-class="is-active"> Notes </RouterLink>
           <RouterLink to="/stats" class="navbar-item" active-class="is-active"> Stats </RouterLink>
@@ -17,4 +34,12 @@
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
