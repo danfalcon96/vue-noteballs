cf63f5171d7d7330326c89268c351eca9d0cef28
Dan Falcon <dan_1196@yahoo.com>
2025-07-19 18:29:40 +0800

    style: set proper padding

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@ import NavBar from '@/components/layout/NavBar.vue'
 
 <template>
   <NavBar />
-  <RouterView />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
 <style>
