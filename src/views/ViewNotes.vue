160e2a1f13a699e79bf9466fce95fb102f324c68
Dan Falcon <dan_1196@yahoo.com>
2025-07-25 16:13:47 +0800

    feat: list notes using cards

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -2,7 +2,19 @@
 
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aut autem commodi
+          consectetur dicta eligendi incidunt, ipsum natus non saepe! Ad autem delectus enim eveniet
+          explicabo laboriosam quidem quis vel.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
 
