62ea68d8dc800a29c510c7250171f92730f80e46
Dan Falcon <dan_1196@yahoo.com>
2025-08-13 20:05:37 +0800

    feat: show the number of characters in a note

--- src/components/notes/Note.vue
+++ src/components/notes/Note.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 const props = defineProps({
   note: {
     type: Object,
     required: true,
   },
+})
+
+const characterLength = computed(() => {
+  let length = props.note.content.length
+  return `${length} character${length > 1 ? 's' : ''}`
 })
 </script>
 
@@ -12,6 +19,9 @@ const props = defineProps({
     <div class="card-content">
       <div class="content">
         {{ note.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
+        </div>
       </div>
     </div>
     <footer class="card-footer">
