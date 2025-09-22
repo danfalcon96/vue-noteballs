3ef08fecea09181c1970234c3e44abe682e581ab
Dan Falcon <dan_1196@yahoo.com>
2025-09-22 21:46:32 +0800

    create reusable component for input of note

---
+++ src/components/notes/AddEditNote.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="card has-background-success-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea
+          :value="modelValue"
+          @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+          class="textarea"
+          placeholder="Write a note here"
+        />
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="form-button"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
