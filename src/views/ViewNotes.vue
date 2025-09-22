89530089da8084315918cdb8b2e1ac9059aadc63
Dan Falcon <dan_1196@yahoo.com>
2025-09-22 21:47:11 +0800

    use reusable note input component for new note

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,46 +1,27 @@
 <script setup lang="ts">
-import { ref, useTemplateRef } from 'vue'
+import { ref } from 'vue'
 import Note from '@/components/notes/Note.vue'
 import { useStoreNotes } from '@/stores/storeNotes.ts'
+import AddEditNote from '@/components/notes/AddEditNote.vue'
 
-const newNoteBox = useTemplateRef('new-note-box')
 const newNote = ref('')
 const storeNotes = useStoreNotes()
 
 const addNote = () => {
   storeNotes.addNote(newNote.value)
   newNote.value = ''
-  newNoteBox.value?.focus()
 }
 </script>
 
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea
-            class="textarea"
-            v-model="newNote"
-            ref="new-note-box"
-            placeholder="Write a note here"
-          />
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            class="button is-link has-background-success"
-            :disabled="!newNote"
-            @click="addNote"
-          >
-            Add New Note
-          </button>
-        </div>
-      </div>
-    </div>
-
+    <AddEditNote v-model="newNote">
+      <template #form-button>
+        <button @click="addNote" :disabled="!newNote" class="button is-link has-background-success">
+          Add Note
+        </button>
+      </template>
+    </AddEditNote>
     <Note v-for="note in storeNotes.notes" :key="note.id" :note />
   </div>
 </template>
